23b1f0ace50d3903893620342e0a0b997d4bfd11
Jassimrtkz <rtkz0357@leeds.ac.uk>
2025-11-28 23:49:08 +0000

    funcs.h

--- funcs.h
+++ funcs.h
@@ -1,9 +1,12 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-void menu_item_1(void);
-void menu_item_2(void);
-void menu_item_3(void);
-void menu_item_4(void);
+// menu items used in main.c
+void menu_item_1(void);   // signal analyser
+void menu_item_2(void);   // ADC converter
+void menu_item_3(void);   // RC filter calculator
+void menu_item_4(void);   // unit converter
+void menu_item_5(void);   // resistor colour code
+void menu_item_6(void);   // AI helper
 
 #endif
